2fc5d61e353f68ba9be91507f157079243f878a2
Cside <cside.story@gmail.com>
2025-03-05 18:03:28 +0900

    fix

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,9 +1,14 @@
 import { HTTPError } from "ky";
 import { fetchUnreadCount } from "../background/fn/fetchUnreadCount";
 
+import { setBadgeNumber } from "../background/fn/setBadgeNumber";
 import "./style.css";
 
 const STATUS_CODE_BAD_REQUEST = 400;
+
+// prod で未読消化ができない場合がある。
+// ソースが古いせいかもしれないが、念の為
+await setBadgeNumber(0);
 
 try {
   await fetchUnreadCount();
