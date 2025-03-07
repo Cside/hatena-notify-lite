f20b6d58d019d17dc5993a73d3bf59ea8ded4f5e
Cside <cside.story@gmail.com>
2025-03-07 17:05:26 +0900

    切り出し

--- entrypoints/background/axiosInstance.ts
+++ entrypoints/background/axiosInstance.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from "axios";
+import axios from "axios";
 import axiosRetry from "axios-retry";
+import { axiosLogging } from "./axiosLogging";
 
 const MAX_RETRIES = 0; // 3; // FIXME
 const RETRY_INTERVAL = 1; // FIXME 5_000;
@@ -15,86 +16,6 @@ axiosRetry(instance, {
   retryDelay: () => RETRY_INTERVAL,
 });
 
-interface AxiosConfigWithStartedAt extends InternalAxiosRequestConfig {
-  startedAt: number;
-}
-
-instance.interceptors.request.use((config) => {
-  (config as AxiosConfigWithStartedAt).startedAt = Date.now();
-  return config;
-});
-
-instance.interceptors.response.use(
-  (response) => responseLogger(response),
-  (error) => errorLogger(error),
-);
+axiosLogging(instance);
 
 export { instance as axiosInstance };
-
-// ==================================================
-// Logger
-// ==================================================
-
-const COLOR_FOR = {
-  SUCCESS: "darkcyan",
-  ERROR: "darkgoldenrod",
-};
-
-const logResponse = (res: AxiosResponse, color: string) =>
-  log(
-    (res.config as AxiosConfigWithStartedAt).startedAt,
-    res.status,
-    res.request?.method,
-    res.request?.url,
-    color,
-  );
-
-const log = (
-  startedAt: number,
-  status: number | string,
-  method: string,
-  url: string,
-  color: string,
-) => {
-  const date = new Date();
-  console.info(
-    // biome-ignore lint/style/useTemplate:
-    "%c" +
-      [
-        date.toLocaleTimeString("ja-JP"),
-        `${date.getTime() - startedAt}ms`,
-        status,
-        method,
-        url,
-      ].join("  "),
-    `color: ${color}`,
-  );
-};
-
-const responseLogger = (res: AxiosResponse): AxiosResponse => {
-  logResponse(res, COLOR_FOR.SUCCESS);
-  return res;
-};
-
-const errorLogger = (error: unknown): unknown => {
-  if (error instanceof AxiosError) {
-    if (error.response) {
-      logResponse(error.response, COLOR_FOR.ERROR);
-    } else {
-      log(
-        (error.config as AxiosConfigWithStartedAt).startedAt,
-        error.code === "ETIMEDOUT"
-          ? "Timeout"
-          : error.code === "ERR_NETWORK"
-            ? "NetworkError"
-            : (error.code ?? "UndefinedCode"),
-        error.request?.method ?? "UndefinedMethod",
-        error.request?.url ?? "UndefinedUrl",
-        COLOR_FOR.ERROR,
-      );
-    }
-  } else {
-    console.error(`Unknown error: ${error}`);
-  }
-  return error;
-};
---
+++ entrypoints/background/axiosLogging.ts
@@ -0,0 +1,86 @@
+import {
+  AxiosError,
+  type AxiosInstance,
+  type AxiosResponse,
+  type InternalAxiosRequestConfig,
+} from "axios";
+
+interface AxiosConfigWithStartedAt extends InternalAxiosRequestConfig {
+  startedAt: number;
+}
+
+const COLOR_FOR = {
+  SUCCESS: "darkcyan",
+  ERROR: "darkgoldenrod",
+};
+
+export const axiosLogging = (instance: AxiosInstance) => {
+  instance.interceptors.request.use((config) => {
+    (config as AxiosConfigWithStartedAt).startedAt = Date.now();
+    return config;
+  });
+
+  instance.interceptors.response.use(
+    (response) => responseLogger(response),
+    (error) => errorLogger(error),
+  );
+};
+
+const responseLogger = (res: AxiosResponse): AxiosResponse => {
+  logResponse(res, COLOR_FOR.SUCCESS);
+  return res;
+};
+
+const errorLogger = (error: unknown): unknown => {
+  if (error instanceof AxiosError) {
+    if (error.response) {
+      logResponse(error.response, COLOR_FOR.ERROR);
+    } else {
+      log(
+        (error.config as AxiosConfigWithStartedAt).startedAt,
+        error.code === "ETIMEDOUT"
+          ? "Timeout"
+          : error.code === "ERR_NETWORK"
+            ? "NetworkError"
+            : (error.code ?? "UndefinedCode"),
+        error.request?.method ?? "UndefinedMethod",
+        error.request?.url ?? "UndefinedUrl",
+        COLOR_FOR.ERROR,
+      );
+    }
+  } else {
+    console.error(`Unknown error: ${error}`);
+  }
+  return error;
+};
+
+const logResponse = (res: AxiosResponse, color: string) =>
+  log(
+    (res.config as AxiosConfigWithStartedAt).startedAt,
+    res.status,
+    res.request?.method,
+    res.request?.url,
+    color,
+  );
+
+const log = (
+  startedAt: number,
+  status: number | string,
+  method: string,
+  url: string,
+  color: string,
+) => {
+  const date = new Date();
+  console.info(
+    // biome-ignore lint/style/useTemplate:
+    "%c" +
+      [
+        date.toLocaleTimeString("ja-JP"),
+        `${date.getTime() - startedAt}ms`,
+        status,
+        method,
+        url,
+      ].join("  "),
+    `color: ${color}`,
+  );
+};
